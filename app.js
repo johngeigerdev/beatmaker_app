8944451f5dc69cebf28d14d43cb61ed401bc6ee3
John <jgeiger48@gmail.com>
2021-12-24 13:19:29 -0500

    play stop button working

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
         this.bpm = 150;
         this.isPlaying = null;
         this.selects = document.querySelectorAll('select');
+        this.muteBtns = document.querySelectorAll('.mute');
     }
 
     //Methods
@@ -83,6 +84,35 @@ class DrumKit {
             
         }
     }
+    mute(e) {
+        const muteIndex = e.target.getAttribute('data-track');
+        e.target.classList.toggle('active');
+        if (e.target.classList.contains('active')) {
+            switch(muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 0;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 0;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 0;
+                    break;
+            }
+        } else {
+            switch(muteIndex) {
+                case "0":
+                    this.kickAudio.volume = 1;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 1;
+                    break;
+                case "2":
+                    this.hihatAudio.volume = 1;
+                    break;
+            }
+        }
+    }
 }
 
 const drumKit = new DrumKit();
@@ -103,5 +133,10 @@ drumKit.playBtn.addEventListener('click', function () {
 drumKit.selects.forEach(select => {
     select.addEventListener('change', function(e) {
         drumKit.changeSound(e)
+    });
+});
+drumKit.muteBtns.forEach(btn => {
+    btn.addEventListener("click", function(e) {
+        drumKit.mute(e);
     });
 });
